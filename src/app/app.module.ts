018e8c7d777bb9491c2bd3488203dc3c5af6b2cc
Gustavo Alves <gustavo.almeida13@fatec.sp.gov.br>
2021-12-24 20:06:06 -0300

    Prepare app for Heroku

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { FormsModule } from "@angular/forms";
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
