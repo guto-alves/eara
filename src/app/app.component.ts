7cc0d76ccdc4db9bdee8ef2c21ed3576f5c00ef4
Gustavo Alves <gustavo.almeida13@fatec.sp.gov.br>
2021-12-24 18:51:38 -0300

    Add  logout button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from './_services/authentication.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private router: Router, private authenticationService: AuthenticationService) { }
+
+  ngOnInit(): void {
+
+  }
+
+  logout(): void {
+    this.authenticationService.logout();
+  }
 }
